2197e1ccff8fe51c86d21b2018d38ca54fc8471d
bpintos <74078770+bpintos@users.noreply.github.com>
2023-12-20 21:17:16 +0100

    Add files via upload

--- coppelia/startupCoppelia.py
+++ coppelia/startupCoppelia.py
@@ -7,8 +7,18 @@ Created on Sat Nov 13 13:11:36 2021
 """
 
 import os
+import sys
+
+mode = sys.argv[1]
+scene = sys.argv[2]
 
 os.chdir('../../CoppeliaSim')
 # os.system('./coppeliaSim.sh /home/bpintos/Projects/smart-trajectory-planning/vehiclegym/envs/scene/PioneerScene.ttt')
 # os.system('./coppeliaSim.sh /home/bpintos/Projects/smart-trajectory-planning/vehiclegym/envs/scene/b1_scene.ttt')
-os.system('./coppeliaSim.sh /home/bpintos/Projects/smart-trajectory-planning/vehiclegym/envs/scene/Pionner_room_obstacles.ttt')
+# os.system('./coppeliaSim.sh /home/bpintos/Projects/smart-trajectory-planning/vehiclegym/envs/scene/Pionner_room_obstacles.ttt')
+
+scene_command = './coppeliaSim.sh ' + mode + ' /home/bpintos/Projects/smart-trajectory-planning/vehiclegym/envs/scene/'\
+    + scene
+
+print(scene_command)
+os.system(scene_command)
